b146f2ae63fc1fb572a45f5d5d016434e3c57197
sckim <sckim@wins21.co.kr>
2018-12-07 09:16:08 +0900

    Updates

---
+++ precache-manifest.cafccd38ed33ce9ffef24b2d54477330.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "93648942626a37703c2a",
+    "url": "/react_prac/static/js/runtime~main.93648942.js"
+  },
+  {
+    "revision": "8a2d75b7a6d61b9bac6b",
+    "url": "/react_prac/static/js/main.8a2d75b7.chunk.js"
+  },
+  {
+    "revision": "7c8f41d282a7da190721",
+    "url": "/react_prac/static/js/1.7c8f41d2.chunk.js"
+  },
+  {
+    "revision": "8a2d75b7a6d61b9bac6b",
+    "url": "/react_prac/static/css/main.b0375f75.chunk.css"
+  },
+  {
+    "revision": "370768a2fc9d11e55f409835071c8fc1",
+    "url": "/react_prac/index.html"
+  }
+];
